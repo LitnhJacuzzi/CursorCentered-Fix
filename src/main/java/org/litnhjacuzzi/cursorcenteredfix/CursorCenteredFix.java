d13eabb44d08cbb3379d47cb9ae694048291b103
LitnhJacuzzi <2075397019@qq.com>
2024-11-21 14:34:21 +0800

    Support wayland(experimental).

--- src/main/java/org/litnhjacuzzi/cursorcenteredfix/CursorCenteredFix.java
+++ src/main/java/org/litnhjacuzzi/cursorcenteredfix/CursorCenteredFix.java
@@ -1,9 +1,22 @@
 package org.litnhjacuzzi.cursorcenteredfix;
 
+import com.sun.jna.Platform;
+
 import net.fabricmc.api.ClientModInitializer;
 
 public class CursorCenteredFix implements ClientModInitializer
 {
+	public static final boolean IS_WAYLAND;
+	
 	@Override
 	public void onInitializeClient() {}
+	
+	static {
+		if(!Platform.isLinux()) {
+			IS_WAYLAND = false;
+		}else {
+			String displayProt = System.getenv("XDG_SESSION_TYPE");
+			IS_WAYLAND = displayProt != null && displayProt.equals("wayland");
+		}
+	}
 }
--- src/main/java/org/litnhjacuzzi/cursorcenteredfix/mixin/MouseMixin.java
+++ src/main/java/org/litnhjacuzzi/cursorcenteredfix/mixin/MouseMixin.java
@@ -1,5 +1,6 @@
 package org.litnhjacuzzi.cursorcenteredfix.mixin;
 
+import org.litnhjacuzzi.cursorcenteredfix.CursorCenteredFix;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,16 +10,37 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.Mouse;
+import net.minecraft.client.util.Window;
 
 @Mixin(Mouse.class)
-public class MouseMixin {
+public abstract class MouseMixin {
 	@Shadow
 	private MinecraftClient client;
+	
+	private final ProcessBuilder setCursorPosProcBuilder = new ProcessBuilder("sudo", "ydotool", "mousemove", "0", "0");
 	
 	@Inject(method = "unlockCursor()V", at = @At(value = "INVOKE", 
 			target = "Lnet/minecraft/client/util/InputUtil;setCursorParameters(JIDD)V"))
 	public void setMouseMode(CallbackInfo ci) {
 		GLFW.glfwSetInputMode(((MinecraftClientMixin) client).getWindow().getHandle(), 
 				GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL);
+	}
+	
+	@Inject(method = "unlockCursor()V", at = @At(value = "INVOKE", 
+			target = "Lnet/minecraft/client/util/InputUtil;setCursorParameters(JIDD)V", shift = At.Shift.AFTER))
+	public void setCursorPos(CallbackInfo ci) {
+		if(CursorCenteredFix.IS_WAYLAND) {
+			Window window = ((MinecraftClientMixin) client).getWindow();
+			String xArg = String.valueOf(window.getX() + window.getWidth() / 2);
+			String yArg = String.valueOf(window.getY() + window.getHeight() / 2);
+			setCursorPosProcBuilder.command().set(3, xArg);
+			setCursorPosProcBuilder.command().set(4, yArg);
+			try {
+				Process setCursorPosProc = setCursorPosProcBuilder.start();
+				setCursorPosProc.waitFor();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
